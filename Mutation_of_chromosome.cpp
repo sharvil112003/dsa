adfc0184d111e05a58934d33667fc1d3ceeb04c5
Sharvil Nichat <sharvilnichat01@gmail.com>
2023-11-29 23:19:27 +0530

    Update Mutation_of_chromosome.cpp

--- Mutation_of_chromosome.cpp
+++ Mutation_of_chromosome.cpp
@@ -1,49 +1,63 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
+int calculateFitness(vector<vector<int>>& distanceMatrix, vector<int>& chromosomes) {
+    int fitness = 0;
 
-vector<int> tspMutation(const vector<int>& chromosome, double mutationRate) {
-    vector<int> mutatedChromosome = chromosome;
-    srand(time(0)); // Seed for random number generation
+    for (int i = 0; i < chromosomes.size() - 1; i++) {
+        int city1 = chromosomes[i];
+        int city2 = chromosomes[i + 1];
 
-    for (int i = 0; i < mutatedChromosome.size(); i++) {
-        // Perform mutation with probability mutationRate
-        if (rand() / static_cast<double>(RAND_MAX) < mutationRate) {
-            // Swap two random cities
-            int j = rand() % mutatedChromosome.size();
-            swap(mutatedChromosome[i], mutatedChromosome[j]);
-        }
+        fitness += distanceMatrix[city1][city2];
     }
 
-    return mutatedChromosome;
+    fitness += distanceMatrix[chromosomes.back()][chromosomes[0]];
+
+    return fitness;
+}
+
+void mutateChromosomes(vector<int>& chromosomes) {
+    srand(time(0));
+
+    int pos1 = rand() % chromosomes.size();
+    int pos2 = rand() % chromosomes.size();
+
+    int temp = chromosomes[pos1];
+    chromosomes[pos1] = chromosomes[pos2];
+    chromosomes[pos2] = temp;
 }
 
 int main() {
-    // Example usage
     vector<vector<int>> distanceMatrix = {
         {0, 10, 15, 20},
-        {10, 0, 35, 25},
-        {15, 35, 0, 30},
-        {20, 25, 30, 0}
+        {5, 0, 9, 10},
+        {6, 13, 0, 12},
+        {8, 8, 9, 0}
     };
 
-vector<int> initialChromosome = {0, 1, 2, 3};  // Initial order of cities
-    double mutationRate = 0.5;  // Probability of mutation for each gene
+    vector<int> chromosomes = {0, 1, 2, 3};
 
-vector<int> mutatedChromosome = tspMutation(initialChromosome, mutationRate);
-
-    cout << "Initial Chromosome: ";
-    for (int city : initialChromosome) {
-        cout << city << " ";
+    cout << "Chromosomes: ";
+    for (int i : chromosomes) {
+        cout << i << " ";
     }
     cout << endl;
 
-    cout << "Mutated Chromosome: ";
-    for (int city : mutatedChromosome) {
-        cout << city << " ";
+    cout << "Fitness of Chromosomes: " << calculateFitness(distanceMatrix, chromosomes) << endl;
+
+    mutateChromosomes(chromosomes);
+
+    cout << "Chromosomes: ";
+    for (int i : chromosomes) {
+        cout << i << " ";
     }
     cout << endl;
+
+    cout << "Fitness of Chromosomes: " << calculateFitness(distanceMatrix, chromosomes) << endl;
 
     return 0;
 }
